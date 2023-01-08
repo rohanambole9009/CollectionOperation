5304c469774e423cfce6720eef6ea0e3f9b61b44
rohanambole <rohanambole3452@gmail.com>
2023-01-08 17:10:47 +0530

    first commit

---
+++ src/com/collection/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		List<String>list=new ArrayList<String>();
+		list.add("manisha");
+		list.add("rohan");
+		list.add("akshay");
+		System.out.println(list);
+
+	}
+	
+}
---
+++ src/com/collection/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package com.collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		List<Integer>list=new LinkedList<Integer>();
+		list.add(34);
+		list.add(45);
+		list.add(80);
+		System.out.println(list);
+	}
+}
